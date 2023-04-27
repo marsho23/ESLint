33002d3fae96c89c3baff890755bfa4a99dc13d1
marsho23 <126670877+marsho23@users.noreply.github.com>
2023-04-27 10:00:52 +0100

    Add files via upload

---
+++ tests/demo.test.js
@@ -0,0 +1,18 @@
+const { expect } = require('chai');
+// eslint-disable-next-line import/extensions
+
+// eslint-disable-next-line no-undef
+describe('test suite', () => {
+  // eslint-disable-next-line no-undef
+  it('should equal 2', () => {
+    expect(1 + 1).to.equal(2);
+  });
+  // it("should not be prime",()=>{
+  //   expect(checkPrime(1).to.equal("false"));
+  // })
+});
+
+// describe("test suite", () => {
+//   it("should equal jimmy", () => {
+//     expect("").to.equal("jimmy");//   });
+// });
